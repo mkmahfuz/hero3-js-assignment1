f9fd1a45ab92c4abfd53981913123ed92f8099c4
mahfuz <mdmahfuzbd@gmail.com>
2021-01-22 05:11:34 +0600

    input validation for hotelCost() and fraction validation added

--- assignment.js
+++ assignment.js
@@ -5,6 +5,7 @@ function kilometerToMeter(km){
     if(isNaN(km) || km < 0){
         return "Invalid input, please try again ";
     }
+
     // 1 kilometer == 1000 meter
     var meter = km * 1000;
     return meter;
@@ -25,16 +26,21 @@ function kilometerToMeter(km){
 
 function budgetCalculator(watch,phone,laptop){
 
-    //input validation (this portion is for assignment's bonus work)    
-    if(isNaN(watch) || watch < 0){
+    //input validation (this portion is for assignment's bonus work)
+    var watchFraction = watch - Math.floor(watch);     
+    var phoneFraction = phone - Math.floor(phone);     
+    var laptopFraction = laptop - Math.floor(laptop);
+
+    if(isNaN(watch) || watch < 0 || watchFraction > 0){
         return "Invalid watch unit, please try again. ";
     }
-    if(isNaN(phone) || phone < 0){
+    if(isNaN(phone) || phone < 0 || phoneFraction > 0){
         return "Invalid phone unit, please try again. ";
     }
-    if(isNaN(laptop) || laptop < 0){
+    if(isNaN(laptop) || laptop < 0 || laptopFraction > 0){
         return "Invalid laptop unit, please try again. ";
     } 
+    //input validation end
 
     // variable initialization
     const watchPrice = 50, phonePrice = 100, laptopPrice = 500;
@@ -57,10 +63,19 @@ function budgetCalculator(watch,phone,laptop){
 // console.log(budgetCalculator(1,1)); //return invalid laptop unit
 // console.log(budgetCalculator(1,1,1,1)); //return 650
 // console.log(budgetCalculator()); //return invalid watch
+// console.log(budgetCalculator(0,0,.1)); //return invalid laptop unit
+// console.log(budgetCalculator(1,1.4,1)); //return invalid phone unit
 
 // function hotelCost : 1 parameter, input number, output number
 
 function hotelCost(days){
+    //input validation (this portion is for assignment's bonus work)
+    if(isNaN(days) || days < 0){
+        return "Invalid input, please try again ";
+    }
+    //fraction of day will be counted as full day (this portion is also for assignment's bonus work)
+    days = Math.ceil(days);
+
     // variable initialization
     const dayOnetoTenPrice = 100, dayEleventoTwentyPrice = 80, dayTwentyonetoMorePrice = 50;
     var totalCost = 0, daysOn2ndSlot = 0, daysOn3rdSlot = 0;
@@ -86,6 +101,13 @@ function hotelCost(days){
 // console.log(hotelCost(13)); //1240
 // console.log(hotelCost(10)); //1000
 // console.log(hotelCost(20)); //1800
+// console.log(hotelCost(0)); //0
+// console.log(hotelCost(1)); //100
+// console.log(hotelCost(-6)); //invalid input
+// console.log(hotelCost(100)); //5800
+// console.log(hotelCost('q')); //invalid input
+// console.log(hotelCost(.2)); //100
+// console.log(hotelCost()); //invalid input
 
 // function megaFriend : 1 parameter, input array, output string
 
