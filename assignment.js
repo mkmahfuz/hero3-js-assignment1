3d2c2962951b91bfe87a009696b117f1e98bb061
mahfuz <mdmahfuzbd@gmail.com>
2021-01-22 03:15:08 +0600

    function megaFriend added

--- assignment.js
+++ assignment.js
@@ -1,6 +1,7 @@
 //put the github link of the repository of this project
 // function kilometerToMeter : 1 parameter, input number, output number
 function kilometerToMeter(km){
+    // 1 kilometer == 1000 meter
     var meter = km * 1000;
     return meter;
 }
@@ -55,3 +56,35 @@ function hotelCost(days){
 // console.log(hotelCost(13)); //1240
 // console.log(hotelCost(10)); //1000
 // console.log(hotelCost(20)); //1800
+
+// function megaFriend : 1 parameter, input array, output string
+
+function megaFriend(nameArray){
+    // variable initialization
+    var largestName = ""; // nameArray[0]
+    var arrLength =  nameArray.length;
+    var i, element;
+
+    //action
+    for(i = 0; i < arrLength; i++){
+        element = nameArray[i];
+        if(element.length > largestName.length){
+            largestName = element;
+        }
+    }
+
+    // return result
+    return largestName;
+}
+
+// test for megaFriend()
+arr1 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump'];
+arr2 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump','kamala haris'];
+arr3 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump','                     ','kamala haris'];
+arr4 = [];
+arr5 = ['karim'];
+console.log(megaFriend(arr1)); //donaldTrump
+console.log(megaFriend(arr2)); //kamala haris
+console.log(megaFriend(arr3)); //empty data
+console.log(megaFriend(arr4)); //empty data
+console.log(megaFriend(arr5)); //empty data
