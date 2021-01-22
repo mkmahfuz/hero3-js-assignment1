1e7673543efda2da6c753f7081b6b3231254760d
mahfuz <mdmahfuzbd@gmail.com>
2021-01-22 07:05:17 +0600

    input validation for megaFriend added

--- assignment.js
+++ assignment.js
@@ -102,6 +102,8 @@ function hotelCost(days){
 // console.log(hotelCost(23)); //1950
 // console.log(hotelCost(13)); //1240
 // console.log(hotelCost(10)); //1000
+// console.log(hotelCost(9.1)); //1000
+// console.log(hotelCost(10.1)); //1080
 // console.log(hotelCost(20)); //1800
 // console.log(hotelCost(0)); //0
 // console.log(hotelCost(1)); //100
@@ -116,31 +118,55 @@ function hotelCost(days){
 // function megaFriend : 1 parameter, input array, output string
 
 function megaFriend(nameArray){
+
+    //input validation (this portion is for assignment's bonus work)
+    if(!Array.isArray(nameArray)){
+        return "Invalid input: Input is not an array";
+    }
+    if(nameArray.length < 1){
+        return "Invalid input: No element found";
+    }
+    //input validation end
+
     // variable initialization
     var largestName = ""; // nameArray[0]
     var arrLength =  nameArray.length;
-    var i, element;
+    var i, element; 
 
     //get the largestName
     for(i = 0; i < arrLength; i++){
         element = nameArray[i];
+
+        if(typeof element !== 'string'){return "element not string";}//input validation : string only.
+        element = element.trim(); //trim the too many spaces around
+
         if(element.length > largestName.length){
             largestName = element;
         }
-    }
+    } //end for loop
 
     // return result
     return largestName;
 }
 
 // test for megaFriend()
-// arr1 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump'];
-// arr2 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump','kamala haris'];
-// arr3 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump','                     ','kamala haris'];
-// arr4 = [];
-// arr5 = ['karim'];
-// console.log(megaFriend(arr1)); //donaldTrump
-// console.log(megaFriend(arr2)); //kamala haris
-// console.log(megaFriend(arr3)); //empty data
-// console.log(megaFriend(arr4)); //empty data
-// console.log(megaFriend(arr5)); //empty data
+arr1 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump'];
+arr2 = ['rahim','karim','jodu','shefali','jonaki','donaldTrump','kamala haris'];
+arr3 = ['rahim','karim','jodu','shefali','jonaki','donald     Trump','                     ','kamala haris'];
+arr4 = [];
+arr5 = ['karim'];
+arr6 = "hello";
+arr7 = "wow,hel,oka";
+arr8 = [123,1234,12345,12,123456,1234,23456];
+arr9 = ['rahim',1234,'karims',12345,12,123456,1234,23456];
+
+console.log(megaFriend(arr1)); //donaldTrump
+console.log(megaFriend(arr2)); //kamala haris
+console.log(megaFriend(arr3)); //donald   Trump
+console.log(megaFriend(arr4)); //no element found
+console.log(megaFriend(arr5)); //karim
+console.log(megaFriend(arr6)); //not an array
+console.log(megaFriend(arr7)); //not an array
+console.log(megaFriend(arr8)); //123456 //now element not string
+console.log(megaFriend()); //not an array
+console.log(megaFriend(arr9)); //element not string
